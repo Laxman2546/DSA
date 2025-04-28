bc8ee96c9b5c3e3e7ac45e8b221e45167dcc90ae
ELLA LAKSHMAN <laxmannani960@gmail.com>
2025-04-28 20:37:40 +0530

    arrays begin

--- pattern.py
+++ pattern.py
@@ -9,13 +9,3 @@ for i in range(n):
 # largest element in an array 
 
 
-
-arr = [1,2,5,8,8,85,6,8,6565565,8,65,6654646465]
-def large_num():
-    largest_num = arr[0]
-    for i in range(len(arr)):
-        if(arr[i] > largest_num):
-            largest_num=arr[i]
-    return largest_num
-
-print(large_num())
